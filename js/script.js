06a509782fae4657dc55f3a6de2b7cfc126ba8e5
jefersonhoraldo-lab <jefersonhoraldo@gmail.com>
2026-07-02 21:10:37 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('button[type="whatsapp"]');
+button.onclick = function() {
+    window.open('https://wa.me/56579673', '_blank');
+}
+const button2 = document.querySelector('button[type="messenger"]');
+button2.onclick = function() {
+    window.open('https://google.com', '_blank');
+}
+const button3 = document.querySelector('button[type="mensaje"]');
+button3.onclick = function() {
+    window.open('https://facebook.com', '_blank');
+}
+const button4  = document.querySelector('button[type="pedir"]');
+button4.onclick = function() {
+    window.open('https://instagram.com', '_blank');
+}
+const button5  = document.querySelector('button[type="pedir"]');
+button5.onclick = function() {
+    window.open('https://instagram.com', '_blank');
+}
